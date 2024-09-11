cc27a17ba53c7b4f68a1dfee44f3f0175eb49560
Dave Arnold <dave@roknsound.com>
2024-09-11 11:10:23 -0700

    Refactor entrypoint.sh to update GitHub runner configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -m
 export PATH="$PATH:/actions-runner"
-RUNNER_NAME_FULL="$NAMESPACE-$HOSTNAME"
+RUNNER_NAME_FULL="${NAMESPACE}-${HOSTNAME}"
 
 # Cut the name if it is more than 64 characters
 RUNNER_NAME=${RUNNER_NAME_FULL:0:64}
@@ -8,16 +8,16 @@ RUNNER_NAME=${RUNNER_NAME_FULL:0:64}
 # Support for setup-python
 AGENT_TOOLSDIRECTORY=/opt/hostedtoolscache
 cd /actions-runner
-./config.sh \
-    --unattended \
-    --url "${REPO_URL}" \
-    --token "${ACCESS_TOKEN}" \
-    --name "${RUNNER_NAME}" \
-    --runnergroup "${RUNNER_GROUP}" \
-    --labels "${RUNNER_LABELS}" \
-    --disableupdate \
-    --replace \
-    --ephemeral
+command="./config.sh --unattended --url ${REPO_URL} --token ${ACCESS_TOKEN} --name ${RUNNER_NAME} "
+
+# Add runner group if non-empty
+if [ -n "${RUNNER_GROUP}" ]; then
+  command="${command} --runnergroup ${RUNNER_GROUP}"
+fi
+
+command="${command} --labels ${RUNNER_LABELS} --disableupdate --replace --ephemeral"
+echo "Configuring github runner with token..."
+eval ${command}
 
 echo "github runner config with token complete."
 
